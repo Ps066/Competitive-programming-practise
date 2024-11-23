fa598aed4d0209de63b071d8c8c847a832e50416
Prashant shukla <81038824+Ps066@users.noreply.github.com>
2024-11-23 13:42:42 +0000

    Updated

---
+++ Patterns_c++/1st_pattern.cpp
@@ -0,0 +1,27 @@
+// Program to print simple pattern with equal number of rows and columns
+
+// example:- 
+//   * * *
+//   * * *
+//   * * *
+//   3 rows and 3 columns
+// The goal is to make it for n columns 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int i = 0;
+  while(i<n){
+    int j = 0;
+    while(j<n){
+      cout << "*";
+      j += 1;
+    }
+    i += 1;
+    cout << endl;
+  }
+  
+}
